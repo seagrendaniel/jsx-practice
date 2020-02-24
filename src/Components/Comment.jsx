1b5ea626da08b4d9db6254ca1c1788161e4424a9
blackdanamite <seagrendaniel562@gmail.com>
2020-02-24 11:00:58 -0800

    update comment posts with props

---
+++ src/Components/Comment.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import faker from 'faker';
+
+const Comment = (props) => {
+  console.log(props);
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+      </a>
+      </div>
+      <div className="metadata">
+  <span className="date">{props.timeAgo}</span>
+      </div>
+      <div className="text">
+        {props.content}
+      </div>
+    </div>
+  );
+}
+
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import Comment from './Components/Comment';
+
 
 // Create a react component
 const App = () => {
-  return <div>Hi There!</div>
-}
 
-// Take the react component and display on screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
+  return (
+    <div className="ui container comments">
+      <Comment 
+      author="Sam" 
+      timeAgo="Today at 4:55PM"
+      content="great post!"
+      />
+      <Comment 
+      author="Alex" 
+      timeAgo="Today at 12:00PM"
+      content="Not too impressed.."
+      />
+      <Comment 
+      author="Joe" 
+      timeAgo="Yesterday at 11:30AM"
+      content="Whoa!"
+      />
+    </div>
+   )
+}
+    
+    // Take the react component and display on screen
+ReactDOM.render(<App />, document.querySelector('#root')
 );
