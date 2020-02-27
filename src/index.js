0559ecd59da830b4e8e32b5b3a0caad9369360d6
blackdanamite <seagrendaniel562@gmail.com>
2020-02-27 15:02:51 -0800

    add comment inside ApprovalCard

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import Comment from './Components/Comment';
+import ApprovalCard from './Components/ApprovalCard';
 
 
 // Create a react component
@@ -10,28 +11,30 @@ const App = () => {
 
   return (
     <div className="ui container comments">
-      <Comment 
-      author="Sam" 
-      timeAgo="Today at 4:55PM"
-      content="great post!"
-      avatar={faker.image.avatar()}
+      <ApprovalCard>
+        <Comment
+          author="Sam"
+          timeAgo="Today at 4:55PM"
+          content="great post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <Comment
+        author="Alex"
+        timeAgo="Today at 12:00PM"
+        content="Not too impressed.."
+        avatar={faker.image.avatar()}
       />
-      <Comment 
-      author="Alex" 
-      timeAgo="Today at 12:00PM"
-      content="Not too impressed.."
-      avatar={faker.image.avatar()}
-      />
-      <Comment 
-      author="Joe" 
-      timeAgo="Yesterday at 11:30AM"
-      content="Whoa!"
-      avatar={faker.image.avatar()}
+      <Comment
+        author="Joe"
+        timeAgo="Yesterday at 11:30AM"
+        content="Whoa!"
+        avatar={faker.image.avatar()}
       />
     </div>
-   )
+  )
 }
-    
-    // Take the react component and display on screen
+
+// Take the react component and display on screen
 ReactDOM.render(<App />, document.querySelector('#root')
 );
