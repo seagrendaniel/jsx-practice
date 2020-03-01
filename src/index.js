512dbcaf97f76e468ec935c6b222051a8bc42d40
blackdanamite <seagrendaniel562@gmail.com>
2020-02-29 20:51:52 -0800

    refactor App component to class based component

--- src/index.js
+++ src/index.js
@@ -8,17 +8,16 @@ import SeasonsApp from './Components/SeasonsApp';
 
 
 // Create a react component
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return (
-    // <SeasonsApp/>
-    <div>Hi there!</div>
-  )
+class App extends React.Component {
+  
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>
+  }
 }
 
 // Take the react component and display on screen
-ReactDOM.render(<App />, document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
