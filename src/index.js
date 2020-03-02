0d5db98ff26a9b7522949c7fbd1f7870e2ebe18f
blackdanamite <seagrendaniel562@gmail.com>
2020-03-02 12:44:26 -0800

    display seasons text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './Components/SeasonsDisplay'
 
 // import faker from 'faker';
 // import Comment from './Components/Comment';
@@ -10,30 +11,26 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 class App extends React.Component {
-  constructor(props) {
-    super(props);       // ensures that the constructor function inside React.Component still gets called
-    this.state = {
-      lat: null,
-      errorMessage: ''
-    };
 
+  state = {lat: null, errorMessage: ''};
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
+  componentDidUpdate() {
+    console.log('my component updated');
+  }
 
   render() {
     if (this.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     }
 
     return <div>Loading!</div>
