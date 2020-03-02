2e101b1810f53cde286fb2c9b3d9211a71c898a4
blackdanamite <seagrendaniel562@gmail.com>
2020-03-02 13:44:20 -0800

    add LoadingSpinner component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './Components/SeasonsDisplay'
+import LoadingSpinner from './Components/LoadingSpinner'
 
 // import faker from 'faker';
 // import Comment from './Components/Comment';
@@ -33,7 +34,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat}/>
     }
 
-    return <div>Loading!</div>
+    return <LoadingSpinner />
   }
 }
 
