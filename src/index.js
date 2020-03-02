d3f36e6b2756298dcebeb56d396d6dc82c36af50
blackdanamite <seagrendaniel562@gmail.com>
2020-03-01 17:33:20 -0800

    return conditional jsx depending on geolocation error or loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // import faker from 'faker';
 // import Comment from './Components/Comment';
 // import ApprovalCard from './Components/ApprovalCard';
@@ -9,23 +10,33 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);       // ensures that the constructor function inside React.Component still gets called
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ''
     };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        this.setState( {lat: position.coords.latitude} );
+        this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
 
-  render(){
-    return <div>Latitude: {this.state.lat}</div>
+  render() {
+    if (this.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
   }
 }
 
